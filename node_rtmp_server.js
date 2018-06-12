7801c82468c2af6ab46cee5e3be4e234785516b6
Thomas Mout <thomas98mout@gmail.com>
2018-06-12 13:58:13 +0200

    API and certificates
    In this update there has been a change to the API. Both endpoints have some light changes.
    An implementation to the TLS side of the application.

--- node_rtmp_server.js
+++ node_rtmp_server.js
@@ -16,42 +16,45 @@ const context = require('./node_core_ctx');
 const RTMP_PORT = 1935;
 
 const options = {
-	// key: fs.readFileSync('certificate.pem'),
-	// cert: fs.readFileSync('certrequest.csr')
+	key: fs.readFileSync('key.pem', 'utf8'),
+	cert: fs.readFileSync('certificate.pem', 'utf8'),
+	// passphrase: 'seechange',
+	requestCert: false,
+	rejectUnauthorized: false
 };
 
 class NodeRtmpServer {
-  constructor(config) {
-    config.rtmp.port = this.port = config.rtmp.port ? config.rtmp.port : RTMP_PORT;
-    this.tcpServer = Net.createServer(options, (socket) => {
-      let session = new NodeRtmpSession(config, socket);
-      session.run();
-    })
-  }
+	constructor(config) {
+		config.rtmp.port = this.port = config.rtmp.port ? config.rtmp.port : RTMP_PORT;
+		this.tcpServer = Net.createServer(options, (socket) => {
+			let session = new NodeRtmpSession(config, socket);
+			session.run();
+		})
+	}
 
-  run() {
-    this.tcpServer.listen(this.port, '0.0.0.0', () => {
-      Logger.log(`Node Media Rtmp Server started on port: ${this.port}`);
-    });
+	run() {
+		this.tcpServer.listen(this.port, '0.0.0.0', () => {
+			Logger.log(`Node Media Rtmp Server started on port: ${this.port}`);
+		});
 
-    this.tcpServer.on('error', (e) => {
-      Logger.error(`Node Media Rtmp Server ${e}`);
-    });
+		this.tcpServer.on('error', (e) => {
+			Logger.error(`Node Media Rtmp Server ${e}`);
+		});
 
-    this.tcpServer.on('close', () => {
-      Logger.log('Node Media Rtmp Server Close.');
-    });
-  }
+		this.tcpServer.on('close', () => {
+			Logger.log('Node Media Rtmp Server Close.');
+		});
+	}
 
-  stop() {
-    this.tcpServer.close();
-    context.sessions.forEach((session, id) => {
-      if (session instanceof NodeRtmpSession) {
-        session.socket.destroy();
-        context.sessions.delete(id);
-      }
-    });
-  }
+	stop() {
+		this.tcpServer.close();
+		context.sessions.forEach((session, id) => {
+			if (session instanceof NodeRtmpSession) {
+				session.socket.destroy();
+				context.sessions.delete(id);
+			}
+		});
+	}
 }
 
 module.exports = NodeRtmpServer;
