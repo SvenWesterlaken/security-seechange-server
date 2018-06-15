53f60b048c4a657639d125ee3c4f8ca4c37dcafe
Unknown <Maikel_J@Live.nl>
2018-06-15 15:54:33 +0200

    Images are acccessible directly

--- controllers/authorization.js
+++ controllers/authorization.js
@@ -3,11 +3,12 @@ const axios = require('axios');
 const fs = require('fs');
 const https = require('https');
 const User = require('../models/user');
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0"; //accept self-signed requests, not needed if certificate is authorized
 
 module.exports = {
 //Checking client authorization token on TruYou server
   authorize(req, res, next) {
-    var instance = axios.create({ //self-signed certificate probably does not work - api for testing not available yet
+    var instance = axios.create({
       url: config.truYou_api, //working link to check agent = 'http://httpbin.org/user-agent'
       httpsAgent: new https.Agent({ //user agent is used to act on behalf of SeeChange, it gives extra information to the receiver so it can assess the capabilities of the sender
         // ca: fs.readFileSync(`${config.signature}.CA`, "utf8"), //not used, certificate is self-signed
@@ -18,25 +19,24 @@ module.exports = {
     });
 
     const authorizationToken = req.header('Token') || ''; //token is encrypted, decrypting it is not a task for SeeChange
+    const username = req.query.username? req.query.username : req.body.username;
     const truYouApi = config.truYou_api;
-    if (authorizationToken != '') {
+    if (authorizationToken != '' && username != '') {
       const truYou_config = {params: {token: authorizationToken}};
-      instance.get(truYouApi, truYou_config).catch(err => next(err)).then(response => {
-
-        if (response.status === 200) { //token has been verified
-          var username = response.data.username;
-
-          if (username !== undefined || null || '') { //Bad response will not create new documents
-            User.findOneAndUpdate( //checking if user has already used SeeChanged (exists in database)
-              {username: `${username}`},
-              {$setOnInsert: {publicName: `${username}`}}, //if user is new, set public name to username
-              {new: true, upsert: true})  // return new doc if one is upserted, upsert a document if it doesnt exist
-              .catch(err => next(err)).then(user => {
-              console.log(user)
-            });
-          }
+      instance.post(`${truYouApi}` + '\\verify', truYou_config).catch(err => next(err)).then(response => {
+        var tokenUsername = response.data.username;
+        if (response.status === 200 && `${tokenUsername}` === `${username}`) { //token has been verified, bad response will not create new documents
+          User.findOneAndUpdate( //checking if user has already used SeeChanged (exists in database)
+            {username: `${username}`},
+            {$setOnInsert: {publicName: `${username}`}}, //if user is new, set public name to username
+            {new: true, upsert: true})  // return new doc if one is upserted, upsert a document if it doesnt exist
+            .catch(err => next(err)).then(user => {
+            console.log(user)
+          });
+          next();
+        } else {
+          res.status(401).json({error: "Missing token or username"}); //Token is not verified or username in token and request are not equal
         }
-        res.status(response.status).send(response.data); //TruYou will validate the token, SeeChange only redirects the status and data that is given in the response
       });
     } else {
       res.status(401).json({error: "Invalid token"}); //request is denied when a token is not available
