5174c7f9186960e4f9f9b2686a20897b75514cf2
Unknown <Maikel_J@Live.nl>
2018-06-15 12:41:05 +0200

    Post-merge fixing

--- config/env.js
+++ config/env.js
@@ -1,11 +1,22 @@
 const dotenv = require('dotenv');
-const env = {port: process.env.PORT || 8081};
+const env = {
+  port: process.env.PORT || 8081,
+  dbHost: process.env.DB_HOST || 'localhost',
+  dbPort: process.env.DB_PORT || '',
+  dbUser: process.env.DB_USER || '',
+  dbPassword: process.env.DB_PASSWORD || '',
+  dbDatabase: process.env.DB_DATABASE || 'SeeChange',
+};
 
 result = dotenv.config();
 
 if (result.error) {
   throw result.error;
 }
+
+const link = process.env.NODE_ENV === 'production' ?
+  'mongodb://' + env.dbUser + ':' + env.dbPassword + '@' + env.dbHost + ':' + env.dbPort + '/' + env.dbDatabase :
+  'mongodb://localhost/' + env.dbDatabase;
 
 env.truYou_api = process.env.TRUYOU_API || 'http://localhost:8080/api';
 
@@ -20,4 +31,4 @@ env.mongo = {
   }
 };
 
-module.exports = env;
+module.exports = env, link;
