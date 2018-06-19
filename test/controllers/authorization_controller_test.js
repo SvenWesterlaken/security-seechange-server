70422cf84652e145afbd58d1ff5383127f508054
matsgemmeke <matsgemmeke@gmail.com>
2018-06-19 15:52:49 +0200

    unit test crash fix

--- test/controllers/authorization_controller_test.js
+++ test/controllers/authorization_controller_test.js
@@ -1,7 +1,6 @@
 const chai = require('chai');
 const chai_http = require('chai-http');
 const path = require('path');
-const server = require('../../index');
 const expect = chai.expect;
 const config = require('../../config/env');
 var mockAdapter = require('axios-mock-adapter');
@@ -11,34 +10,36 @@ chai.use(chai_http);
 
 describe('Client authorization', () => {
 
-  var mock = new mockAdapter(axios);
-  mock.onAny(path.normalize(`${config.truYou_api}` + '/verify/streamer1337')).reply(200, {
-    username: 'streamer1337'
-  });
+    var mock = new mockAdapter(axios);
+    mock.onAny(path.normalize(`${config.truYou_api}` + '/verify/streamer1337')).reply(200, {
+        username: 'streamer1337'
+    });
 
-  var authorizationToken = "token123";
+    var authorizationToken = "token123";
 
-  xit('Token given by client', (done) => {
-    chai.request(server)
-      .get('/api')
-      .set({Token: `${authorizationToken}`})
-      .set('X-Username', 'streamer1337')
-      .end((err, res) => {
-        expect(err).to.be.null;
-        // expect(res).to.have.status(200);
-        // expect(res.body).to.include({username: 'streamer1337'});
-        done();
-      });
-  });
+    xit('Token given by client', (done) => {
+        const server = require('../../index');
+        chai.request(server)
+            .get('/api')
+            .set({Token: `${authorizationToken}`})
+            .set('X-Username', 'streamer1337')
+            .end((err, res) => {
+                expect(err).to.be.null;
+                // expect(res).to.have.status(200);
+                // expect(res.body).to.include({username: 'streamer1337'});
+                done();
+            });
+    });
 
-  xit('No token provided', (done) => {
-    chai.request(server)
-      .get('/api')
-      .set('X-Username', 'streamer1337')
-      .end((err, res) => {
-        expect(err).to.be.null;
-        expect(res).to.have.status(400);
-        done();
-      });
-  });
+    xit('No token provided', (done) => {
+        const server = require('../../index');
+        chai.request(server)
+            .get('/api')
+            .set('X-Username', 'streamer1337')
+            .end((err, res) => {
+                expect(err).to.be.null;
+                expect(res).to.have.status(400);
+                done();
+            });
+    });
 });
